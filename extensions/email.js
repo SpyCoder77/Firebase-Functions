11ba5ca776994b51a112c89b00ff5c4ed21270b0
SpyCoder77 <130170051+SpyCoder77@users.noreply.github.com>
2024-02-02 19:02:57 -0500

    Create email.js
    Added a basic extension folder. 
    Added an email extension file.

---
+++ extensions/email.js
@@ -0,0 +1,33 @@
+// This is for the extension "Trigger email with firestore". https://extensions.dev/extensions/firebase/firestore-send-email
+// Make sure to install it and configure it with the mailing collection being "mail"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getFirestore, collection, addDoc, doc, onSnapshot, setDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: YOUR_API_KEY,
+  authDomain: YOUR_AUTH_DOMAIN,
+  projectId: YOUR_PROJECT_ID,
+  storageBucket: YOUR_STORAGE_BUCKET,
+  messagingSenderId: YOUR_MESSAGING_SENDER_ID,
+  appId: YOUR_APP_ID,
+};
+
+//Initialize app (Required)
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function sendEmail(recipients, subject, message) {
+    let docId = new Date();
+    await addDoc(collection(db, "mail", docId), {
+        to: recipients,
+        message: {
+            subject: subject,
+            text: message,
+        },
+    });
+    const unsub = onSnapshot(doc(db, "mail", docId), (doc) => {
+        if (doc.data().delivery.info.state == "SUCCESS") {
+            console.log("Email Successfully sent!");
+        }
+    });
+}
